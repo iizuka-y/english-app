45c38cd4908e25a7592ffcd8705fb7603b4cd1f7
iizuka <mesicharida202@gmail.com>
2019-12-28 16:29:49 +0900

    投稿モーダルとかを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+
   def new
     @post = current_user.posts.build
   end
@@ -13,8 +16,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:success] = "投稿を更新しました"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "投稿を削除しました！"
+    redirect_to request.referrer || root_url #request.referrerメソッドは一つ前のURLを返す
+  end
+
   private
     def post_params
-      params.require(:post).permit(:keyword, :content, :information)
+      params.require(:post).permit(:keyword, :content, :information, :image)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
