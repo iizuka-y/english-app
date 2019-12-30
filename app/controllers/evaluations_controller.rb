8c315f5064ea781418ae2e095db6204872e27124
iizuka <mesicharida202@gmail.com>
2019-12-30 22:20:17 +0900

    ajaxによる評価機能を追加

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -2,24 +2,27 @@ class EvaluationsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    evaluation = current_user.evaluations.build(post_id: params[:post_id], star_count: params[:star_count])
-    evaluation.save
-    redirect_to request.referer
-    #respond_to do |format|
-    #  format.html { redirect_to request.referer }
-    #  format.js
-    #end
+    @post = Post.find(params[:post_id])
+    @evaluation = current_user.evaluations.build(post_id: params[:post_id], star_count: params[:star_count])
+    @evaluation.save
+
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
 
   end
 
   def destroy
-    evaluation = Evaluation.find_by(user_id: current_user.id, post_id: params[:post_id])
-    evaluation.destroy
-    redirect_to request.referer
-    #respond_to do |format|
-    #  format.html { redirect_to request.referer }
-    #  format.js
-    #end
+    @post = Post.find(params[:post_id])
+    @evaluation = Evaluation.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @evaluation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
+
   end
 
 end
--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -4,4 +4,20 @@ module EvaluationsHelper
     Evaluation.find_by(user_id: user.id, post_id: post.id).star_count
   end
 
+  def sum_star(post) # あるpostの★の合計数を計算する
+    i = 0
+    post.evaluations.each do |evaluation|
+      i += evaluation.star_count
+    end
+    return i
+  end
+
+  def sum_stars(posts) #たくさんのpostの★の合計数を計算する
+    i = 0
+    posts.each do |post|
+      i += sum_star(post)
+    end
+    return i
+  end
+
 end
